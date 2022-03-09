5d10e4b87fe250d88a94c37fbec6796bca7e4914
boyarov.a <boyarov.a@it-universe.ru>
2022-03-09 12:42:20 +0400

    Application created

---
+++ src/main/java/com/example/personalcalendarbackend/PersonalCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalcalendarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCalendarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalCalendarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/personalcalendarbackend/PersonalCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personalcalendarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalCalendarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
