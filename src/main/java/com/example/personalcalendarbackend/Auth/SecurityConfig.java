ec4f7496fd0c16e0c79c295bdf9c10761dab74b2
boyarov.a <boyarov.a@it-universe.ru>
2022-06-07 11:42:19 +0400

    registration

--- src/main/java/com/example/personalcalendarbackend/Auth/SecurityConfig.java
+++ src/main/java/com/example/personalcalendarbackend/Auth/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .loginProcessingUrl("/login")
+//                .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/redirect", true)
 //                .formLogin(form -> form
 //                        .loginPage("/login")
